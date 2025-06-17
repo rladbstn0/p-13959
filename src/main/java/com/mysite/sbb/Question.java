db21332a9e0f323f722655dfbf944c4100c439d9
user.email <younsoo6103@naver.com>
2025-06-17 14:47:19 +0900

    work

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -1,11 +1,15 @@
 package com.mysite.sbb;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Question {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,6 +24,6 @@ public class Question {
     private String content;
 
     //mappedBy 속성을 지정하지 않으면 중간테이블을 만든다.
-    @OneToMany(cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answer;
 }
