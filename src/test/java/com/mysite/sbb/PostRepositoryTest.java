240321addc31234f0a2f37ac65a4737864fef209
user.email <younsoo6103@naver.com>
2025-06-17 15:25:48 +0900

    work

--- src/test/java/com/mysite/sbb/PostRepositoryTest.java
+++ src/test/java/com/mysite/sbb/PostRepositoryTest.java
@@ -47,4 +47,14 @@ class PostRepositoryTest {
         Question question = questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.").get();
         assertThat(question.getId()).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("findBySubjectLike")
+    void t5() {
+        List<Question> questions = questionRepository.findBySubjectLike("sbb%");
+        assertThat(questions).hasSize(1);
+
+        Question question = questions.get(0);
+        assertThat(question.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
 }
